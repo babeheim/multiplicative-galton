b2e4ff9e483c101d086ab3a24d98054fa5744899
Bret Beheim <beheim@gmail.com>
2019-05-19 21:21:59 +0200

    minor refactorings

--- sim_board.r
+++ sim_board.r
@@ -1,4 +1,27 @@
 
+rm(list = ls())
+
+gradient_maker <- function(start=NA, stop=NA, cols=c("darkorange", "white", "darkcyan"), vis=FALSE, n=1000){
+    if(is.na(start) | is.na(stop)) stop("need to specify start and stop points on a numerical scale")
+    colfunc <- colorRampPalette(cols)
+    color.list <- colfunc(n)
+    color.locations <- seq(start, stop, length=n)
+    names(color.locations) <- color.list
+    if(vis==TRUE) plot(color.locations, rep(1, n), col=color.list, pch="|", ylim=c(0.9, 1.1), cex=5)
+    return(color.locations)
+}
+
+data_gradient <- function(data, colors=c("darkorange", "white", "darkcyan"), my.start=NA, my.stop=NA){
+    if(is.na(my.start)) my.start <- min(data, na.rm=TRUE)
+    if(is.na(my.stop)) my.stop <- max(data, na.rm=TRUE)
+    my.gradient <- gradient_maker(start=my.start, stop=my.stop, cols=colors)
+    if(any(data > max(my.gradient), na.rm=T) | any(data < min(my.gradient), na.rm=T)) warning("data is not within gradient range")
+    data.colors <- rep(NA, length(data))
+    for(i in 1:length(data)){
+        if(!is.na(data[i])) data.colors[i] <- names(my.gradient)[which.min(abs(data[i]-my.gradient))]
+    }
+    data.colors
+}
 
 col_alpha <- function (acol, alpha = 0.2){
     acol <- col2rgb(acol)
@@ -30,34 +53,33 @@ dir_init <- function(path, verbose=FALSE, overwrite=TRUE){
   }
 }
 
-# we need to be able to truncate the base to allow the balls to fall within!
+################
 
-right_line <- function(a, b, c, frac = 1) {
-  x <- seq(a, a * c^frac, length.out = 20)
-  y <- b + log(a)/log(c) - log(x)/log(c)
+calc_right_line <- function(a, b, lambda, height = 1) {
+  x <- seq(a, a * lambda^height, length.out = 20)
+  y <- b + (log(a) - log(x)) / log(lambda)
   out <- list(x = x, y = y)
   return(out)
 }
 
-left_line <- function(a, b, c, frac = 1) {
-  x <- seq(a, a*(1/c)^frac, length.out = 20)
-  y <- b + log(a)/log(1/c) - log(x)/log(1/c)
+calc_left_line <- function(a, b, lambda, height = 1) {
+  x <- seq(a, a * (1 / lambda)^height, length.out = 20)
+  y <- b + (log(a) - log(x)) / log(1 / lambda)
   out <- list(x = x, y = y)
   return(out)
 }
 
-draw_triangle <- function(a, b, c, frac, ...) {
+draw_triangles <- function(a = 1, b, lambda, height = 1, ...) {
   if (length(a) == 1) {
-    left <- left_line(a, b, c, frac)
-    right <- right_line(a, b, c, frac)
-    bottom <- list(x = seq(a * (1/c)^frac, a * c^frac, length.out = 20),
-     y = rep(b - frac * 1, 20))
-    shape <- list(x = c(left$x, bottom$x, rev(right$x)),
-      y = c(left$y, bottom$y, rev(right$y)))
-    polygon(shape, ...)
+    left_line <- calc_left_line(a, b, lambda, height)
+    right_line <- calc_right_line(a, b, lambda, height)
+    triangle_shape <- list(x = c(left_line$x, rev(right_line$x)),
+      y = c(left_line$y, rev(right_line$y)))
+    polygon(triangle_shape, ...)
   }
   if (length(a) > 1) {
-    for (i in 1:length(a)) draw_triangle(a[i], b, c, frac, ...)
+    # for drawing a whole row of triangles (all with the same b)
+    for (i in 1:length(a)) draw_triangles(a[i], b, lambda, height, ...)
   }
 }
 
@@ -67,37 +89,41 @@ power_maker <- function(i) {
     out <- c(rev(pos) * (-1), pos)
   }
   if (i %% 2 == 0) {
-    pos <- seq(2, i, by = 2)
-    out <- c(rev(pos) * (-1), 0, pos)
+    if (i == 0) {
+      out <- 0
+    } else {
+      pos <- seq(2, i, by = 2)
+      out <- c(rev(pos) * (-1), 0, pos)
+    }
   }
   return(out)
 }
 
-plot_board <- function(a = 1, b = 10, c = 2.65, n_levels = 10, xlog = FALSE, frac = 1) {
+plot_board <- function(a = 1, b = 10, lambda = 1.15, n_times = 10,
+  xlog = FALSE, height = 1) {
 
-  plot(1, 1, type = "n", ylim = c(b - 2 * n_levels, b),
-    xlim = c(c^(-n_levels), c^(n_levels)), frame.plot = FALSE,
-    axes =  FALSE, ann = FALSE, xlog = xlog)
+  plot(1, 1, type = "n", ylim = c(b - (2 * n_times), b),
+    xlim = c(a * lambda^(-n_times), a * lambda^(n_times)),
+    frame.plot = FALSE, axes =  FALSE, ann = FALSE, xlog = xlog)
 
-  draw_triangle(a, b, c, border = NA, frac = frac, col = col_alpha("blue", 0.2))
-
-  for (i in 1:(n_levels - 1)) {
-    powers <- power_maker(i)
-    draw_triangle(a * c^powers, b - i, c, border = NA, frac = frac,
-      col = col_alpha("blue", 0.2))
+  for (i in 1:n_times) {
+    powers <- power_maker(i - 1)
+    draw_triangles(a * lambda^powers, b - i + 1, lambda, border = NA,
+      height = height, col = col_alpha(gray(0.5), 0.5))
   }
 
 }
 
+# seperate the simulation from the display?
 
-sim_balls <- function(n_balls, start_time, n_levels, a, b, c) {
+sim_balls <- function(n_balls, start_time, n_times, a, b, lambda) {
 
-  final_bins <- a * c^power_maker(n_levels)
+  final_bins <- a * lambda^power_maker(n_times)
   final_bins_counts <- rep(0, length(final_bins))
   hist_scale <- 0.06
-  hist_baseline <- b - 2 * n_levels
+  hist_baseline <- b - 2 * n_times
   
-  n_keyframes <- (n_balls + n_levels + 2)
+  n_keyframes <- (n_balls + n_times + 2)
 
   keyframes <- list()
 
@@ -128,7 +154,7 @@ sim_balls <- function(n_balls, start_time, n_levels, a, b, c) {
       balls$state[starting] <- "starting"
     }
 
-    on_board <- which(balls$start_time < i & balls$active_time <= (n_levels + 1))
+    on_board <- which(balls$start_time < i & balls$active_time <= (n_times + 1))
     if (length(on_board) > 0) {
       balls$y[on_board] <- balls$y[on_board] - 1
       next_move <- sample(c(-1, 1), length(on_board), replace = TRUE)
@@ -136,13 +162,13 @@ sim_balls <- function(n_balls, start_time, n_levels, a, b, c) {
       balls$state[on_board] <- "on_board"
     }
 
-    falling <- which(balls$active_time == (n_levels + 2))
+    falling <- which(balls$active_time == (n_times + 2))
     if (length(falling) > 0) {
-      balls$y[falling] <- b - (n_levels * 1.2)
+      balls$y[falling] <- b - (n_times * 1.2)
       balls$state[falling] <- "falling"
     }
 
-    landing <- which(balls$active_time == (n_levels + 3))
+    landing <- which(balls$active_time == (n_times + 3))
     if (length(landing) > 0) {
       for (j in 1:length(landing)) {
         my_bin <- which(round(final_bins, 3) == round(balls$x[landing[j]], 3))
@@ -152,14 +178,13 @@ sim_balls <- function(n_balls, start_time, n_levels, a, b, c) {
       balls$state[landing] <- "landing"
     }
 
-    resting <- which(balls$active_time >= (n_levels + 4))
+    resting <- which(balls$active_time >= (n_times + 4))
     if (length(resting) > 0) {
       balls$state[resting] <- "resting"
     }
 
     keyframes[[i]] <- balls
   }
-
 
   for (i in 1:n_keyframes) {
     keyframes[[i]]$time <- i
@@ -176,29 +201,6 @@ sim_balls <- function(n_balls, start_time, n_levels, a, b, c) {
 
 
 
-gradient_maker <- function(start=NA, stop=NA, cols=c("darkorange", "white", "darkcyan"), vis=FALSE, n=1000){
-    if(is.na(start) | is.na(stop)) stop("need to specify start and stop points on a numerical scale")
-    colfunc <- colorRampPalette(cols)
-    color.list <- colfunc(n)
-    color.locations <- seq(start, stop, length=n)
-    names(color.locations) <- color.list
-    if(vis==TRUE) plot(color.locations, rep(1, n), col=color.list, pch="|", ylim=c(0.9, 1.1), cex=5)
-    return(color.locations)
-}
-
-data_gradient <- function(data, colors=c("darkorange", "white", "darkcyan"), my.start=NA, my.stop=NA){
-    if(is.na(my.start)) my.start <- min(data, na.rm=TRUE)
-    if(is.na(my.stop)) my.stop <- max(data, na.rm=TRUE)
-    my.gradient <- gradient_maker(start=my.start, stop=my.stop, cols=colors)
-    if(any(data > max(my.gradient), na.rm=T) | any(data < min(my.gradient), na.rm=T)) warning("data is not within gradient range")
-    data.colors <- rep(NA, length(data))
-    for(i in 1:length(data)){
-        if(!is.na(data[i])) data.colors[i] <- names(my.gradient)[which.min(abs(data[i]-my.gradient))]
-    }
-    data.colors
-}
-
-
 ##########
 
 rose_to_cyan <- c(
@@ -209,25 +211,25 @@ rose_to_cyan <- c(
   "#2F9390"
 )
 
-# n_levels = 6, n_balls = 180, hist_scale = 0.1
-# n_levels = 14, n_balls = 180, hist_scale = 0.06
+# n_times = 6, n_balls = 180, hist_scale = 0.1
+# n_times = 14, n_balls = 180, hist_scale = 0.06
 
 a <- 1
 b <- 10
-n_levels <- 14
-c <- 2.65^(1/n_levels)
+n_times <- 14
+c <- 2.65^(1/n_times)
 
-# plot_board(a = a, b = b, c = c, n_levels = n_levels, xlog = FALSE, frac = 1)
+# plot_board(a = a, b = b, c = c, n_times = n_times, xlog = FALSE, frac = 1)
 
 # simulate balls falling through
 
-n_balls <- 1000
+n_balls <- 100
 start_time <- 1:n_balls # each ball starts one after the other
 
 set.seed(1001)
-locations <- sim_balls(n_balls, start_time, n_levels, a, b, c)
+locations <- sim_balls(n_balls, start_time, n_times, a, b, c)
 
-n_keyframes <- (n_balls + n_levels + 2)
+n_keyframes <- (n_balls + n_times + 2)
 
 all_bins <- sort(unique(locations$x))
 all_bins_cols <- data_gradient(all_bins, colors = rev(rose_to_cyan))
@@ -245,7 +247,7 @@ locations$y[tar] <- locations$y[tar] + 0.09
 
 dir_init("./keyframes")
 
-for (i in 700:900) {
+for (i in 1:n_keyframes) {
 
   current_frame <- which(locations$time == i & locations$active_time <= i)
 
@@ -256,11 +258,11 @@ for (i in 700:900) {
   png(filename, res = 300, height = 4, width = 5, units = "in")
   global_mar <- c(4.1, 0, 0, 0)
   par(mar = global_mar)
-  plot_board(a = a, b = b, c = c, n_levels = n_levels, xlog = FALSE, frac = 0.8)
-  points(current_x, current_y, pch = 16, cex = 0.5 * (20/n_levels),
+  plot_board(a = a, b = b, c = c, n_times = n_times, xlog = FALSE, frac = 0.8)
+  points(current_x, current_y, pch = 16, cex = 0.5 * (20/n_times),
     col = col_alpha(locations$ball_col[current_frame], 1))
-  final_bins <- a * c^power_maker(n_levels)
-  axis(1, at = final_bins, labels = power_maker(n_levels))
+  final_bins <- a * c^power_maker(n_times)
+  axis(1, at = final_bins, labels = power_maker(n_times))
   mtext("logarithmic units", 1, line = 2.25)
   dev.off()
 
@@ -277,9 +279,9 @@ for (i in 700:900) {
     tar <- which(locations$state[next_frame_subset] %in% c("starting", "on_board"))
     if (length(tar) > 0) intermediate_y[tar] <- current_y[tar] - 0.3
     tar <- which(locations$state[next_frame_subset] %in% c("falling"))
-    if (length(tar) > 0) intermediate_y[tar] <- b - (n_levels * 1.1)
+    if (length(tar) > 0) intermediate_y[tar] <- b - (n_times * 1.1)
     tar <- which(locations$state[next_frame_subset] %in% c("landing"))
-    if (length(tar) > 0) intermediate_y[tar] <- b - (n_levels * 1.6)
+    if (length(tar) > 0) intermediate_y[tar] <- b - (n_times * 1.6)
 
     intermediate_x <- current_x
     tar <- which(locations$state[next_frame_subset] %in% c("starting", "on_board"))
@@ -289,11 +291,11 @@ for (i in 700:900) {
     png(filename, res = 300, height = 4, width = 5, units = "in")
     global_mar <- c(4.1, 0, 0, 0)
     par(mar = global_mar)
-    plot_board(a = a, b = b, c = c, n_levels = n_levels, xlog = FALSE, frac = 0.8)
-    points(intermediate_x, intermediate_y, pch = 16, cex = 0.5 * (20/n_levels),
+    plot_board(a = a, b = b, c = c, n_times = n_times, xlog = FALSE, frac = 0.8)
+    points(intermediate_x, intermediate_y, pch = 16, cex = 0.5 * (20/n_times),
       col = col_alpha(locations$ball_col[current_frame], 1))
-    final_bins <- a * c^power_maker(n_levels)
-    axis(1, at = final_bins, labels = power_maker(n_levels))
+    final_bins <- a * c^power_maker(n_times)
+    axis(1, at = final_bins, labels = power_maker(n_times))
     mtext("logarithmic units", 1, line = 2.25)
     dev.off()
 
@@ -309,6 +311,6 @@ library(beepr)
 
 system("convert -loop 1 -delay 7 ./keyframes/keyframe* ./test_14.gif")
 
-beep(5)
+beep(4)
 
 
